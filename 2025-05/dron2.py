5e7c9866aceed08531ac950c099fdc46fd505699
Szymon <kssk12jogurt@gmail.com>
2025-10-13 14:30:12 +0200

    zadanie 3 gotowe

--- 2025-05/dron2.py
+++ 2025-05/dron2.py
@@ -1,4 +1,4 @@
-plik = "dron_przyklad.txt"
+plik = "dron.txt"
 with open(plik) as file:
     linie = file.readlines()
 
@@ -7,11 +7,24 @@ linie = filter(lambda x: x, linie)
 pary = map(lambda x: x.split(" "), linie)
 pary = map(lambda x: (int(x[0]), int(x[1])), pary)
 
+punkty = []
 x, y = 0, 0
 ilosc = 0
 for (x_p, y_p) in pary:
     x, y = x + x_p, y + y_p
+    punkty.append((x, y))
     if all([x < 5000, y < 5000, x, y]):
         ilosc += 1
+print(f"a) {ilosc}")
 
-print(f"Ilość punktów w kwadracie = {ilosc}")
+punkty_wynik = None
+for punkt1 in punkty:
+    for punkt2 in punkty:
+        suma_x = punkt1[0] + punkt2[0]
+        suma_y = punkt1[1] + punkt2[1]
+        if not all([suma_x % 2 == 0, suma_y % 2 == 0]): continue
+        srodek = (suma_x // 2, suma_y // 2)
+        if srodek in punkty and srodek not in [punkt1, punkt2]:
+            if not punkty_wynik:
+                punkty_wynik = f"b) {punkt1}, {srodek}, {punkt2}"
+print(punkty_wynik)
