ac8c3cab68a6d77f761ac4f9d34f945a212b9c23
2022aR <kssk12jogurt@gmail.com>
2025-10-20 12:25:43 +0200

    access

--- Pozamaturalne Zadania/Stack/main.py
+++ Pozamaturalne Zadania/Stack/main.py
@@ -3,7 +3,6 @@ from typing import Any
 class Stack:
     def __init__(self, values: list[Any] = []):
         self.values = values
-
     def push(self, value):
         self.values.append(value)
 
@@ -12,3 +11,16 @@ class Stack:
 
     def is_empty(self) -> bool:
         return bool(self)
+
+
+stos = Stack()
+
+stos.push(4)
+stos.push(7)
+stos.push(3)
+stos.push(8)
+
+# stos.pop()
+# stos.pop()
+print(stos.values[1])
+print(stos.pop())
